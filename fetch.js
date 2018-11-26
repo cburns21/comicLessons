2bec7c9edab83d1ab60db5cc35161c381ec682ca
caroline burns <carolineburns@carolines-MacBook.local>
2018-11-26 15:40:33 -0700

    completed some of the strech goals

--- fetch.js
+++ fetch.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
-console.log('dobby is a free elf!')
-    fetch(`https://galvanize-cors.herokuapp.com/https://xkcd.com/1162/info.0.json`)
-
- 
-
-  .then(function(response) {
-    return response.json()
-  
-})
-  .then(function(JSONresponse) {
-    var body = document.querySelector('h1')
-    console.log(JSONresponse)
-    return body.json
+  console.log('dobby is a free elf!')
+  fetch(`https://galvanize-cors.herokuapp.com/https://xkcd.com/1162/info.0.json`)
+    .then(function(response) {
+      return response.json()
+  })
+    .then(function(JSONresponse) {
+      var body = document.querySelector('h1')
+      console.log(JSONresponse)
+     body.innerText = JSONresponse.title
+      var bodyAlt = document.querySelector('p')
+      bodyAlt.innerText = JSONresponse.alt
+      var bodyimg = document.querySelector('img')
+      bodyimg.innerText = JSONresponse.img
   })
 })
+
+function button(){
+    alert('hey, are you listening?')
+}
+
+function button2(){
+    
+}
